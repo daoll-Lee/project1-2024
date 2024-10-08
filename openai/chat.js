71a3012786a020a59f6fa831cb635636594d1821
Daoll <daoll43000@gmail.com>
2024-10-08 12:20:28 +0900

    완성

--- openai/chat.js
+++ openai/chat.js
@@ -1,8 +1,12 @@
 OPENAPI_KEY = ""
+
 smodelmini = "gpt-4o-mini"
-squestion = '황진이가 누구지?'
+smodel4o0 = "gpt-4o"
+    // squestion = '황진이가 누구지?'
+    // squestion = txtMsg.value
 
 function talk() {
+    squestion = txtMsg.value
     data = {
         model: smodelmini,
         messages: [{
@@ -21,12 +25,43 @@ function talk() {
         contentType: "application/json; charset=utf-8"
     }).done(function(response) {
         console.log(response)
-        alert(response.choices[0].message.content)
+            // alert(response.choices[0].message.content)
+        txtOut.value = response.choices[0].message.content
     }).fail(function(error) {
         console.log(error)
             // alert(error.responseJSON.error.code)
         errormsg = error.status + ":" + error.responseJSON.error.code + "-" + error.responseJSON.error.message
-        alert(errormsg)
+            // alert(errormsg)
+        txtOut.value = errormsg
     })
 }
-talk()
+
+
+function draw() {
+    squestion = txtMsg.value
+    data = {
+        prompt: squestion,
+        n: 2,
+        size: "512x512"
+    }
+    $.ajax({
+        type: "POST",
+        url: "https://api.openai.com/v1/images/generations",
+        headers: {
+            "Authorization": "Bearer " + OPENAPI_KEY
+        },
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8"
+    }).done(function(response) {
+        console.log(response)
+            // alert(response.choices[0].message.content)
+        gimage.src = response.data[0].url
+        gimage2.src = response.data[1].url
+    }).fail(function(error) {
+        console.log(error)
+            // alert(error.responseJSON.error.code)
+        errormsg = error.status + ":" + error.responseJSON.error.code + "-" + error.responseJSON.error.message
+            // alert(errormsg)
+        txtOut.value = errormsg
+    })
+}
